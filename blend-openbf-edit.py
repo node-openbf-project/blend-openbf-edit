cb2d35fc890c588a66790981cc4ccae3c78d4f9a
RepComm <rccommandotac@gmail.com>
2020-02-19 20:26:46 -0600

    Added a bunch of features to UI/output required for parent collision shapes

--- blend-openbf-edit.py
+++ blend-openbf-edit.py
@@ -1,5 +1,6 @@
 import bpy
 import json
+from bpy.props import IntProperty, CollectionProperty, BoolProperty, EnumProperty
 from bpy.types import Operator
 from bpy.app.handlers import persistent
 from bpy.app import driver_namespace
@@ -8,7 +9,7 @@ from math import radians
 
 class UnOpenBFifyOperator(Operator):
 	"""Remove 'openbf-data' from an object"""
-	bl_idname = "object.unopenbfify"
+	bl_idname = "object.openbf_remove"
 	bl_label = "Remove OpenBF-data"
 	bl_options = {'REGISTER', 'UNDO'}
 
@@ -16,6 +17,17 @@ class UnOpenBFifyOperator(Operator):
 		active = bpy.context.active_object
 		if "openbf-data" in active:
 			del active["openbf-data"]
+		return {'FINISHED'}
+
+class OpenBFValidateUserDataOperator(Operator):
+	"""Validate 'openbf-data' of an object"""
+	bl_idname = "object.openbf_validate"
+	bl_label = "Validate"
+	bl_options = {'REGISTER', 'UNDO'}
+
+	def execute(self, context):
+		active = bpy.context.active_object
+		copyDataToCustomProps(active)
 		return {'FINISHED'}
 
 def get_verts_edges(nurbs_object, doRound=False, roundTo=4):
@@ -37,6 +49,9 @@ def copyDataToCustomProps (active):
 	if active.rigid_body != None:
 		data["collision"] = {"shape":active.rigid_body.collision_shape, "radius":1}
 		data["physics"] = {"mass":active.rigid_body.mass, "friction":active.rigid_body.friction, "restitution":active.rigid_body.restitution,"dynamic":active.rigid_body.enabled}
+	else:
+		data["isParentCollisionMesh"] = active.openbfextras.isParentCollisionMesh
+		data["parentCollisionShape"] = active.openbfextras.parentCollisionShape
 	if active.type == "CURVE":
 		verts, edges = get_verts_edges(active)
 		data["path"] = {"verts":verts, "edges":edges}
@@ -58,17 +73,17 @@ def copyDataToCustomProps (active):
 			#data["light"]["position"] = active.location
 		#elif active.data.type == "AREA":
 			#Unsupported for now
-	active["openbf-data"] = json.dumps(data)
+	data["hideInGame"] = active.openbfextras.hideInGame
+	active["openbf-data"] = data
 
 class OpenBFifyOperator(Operator):
 	"""OpenBF-ify an object (add custom openbf properties if they don't exist)"""
-	bl_idname = "object.openbfify"
+	bl_idname = "object.openbf_add"
 	bl_label = "OpenBF-ify"
 	bl_options = {'REGISTER', 'UNDO'}
 
 	def execute(self, context):
 		active = bpy.context.active_object
-		
 		copyDataToCustomProps(active)
 		return {'FINISHED'}
 
@@ -82,10 +97,8 @@ class CustomPropOpenBFPanel(bpy.types.Panel):
 
 	def draw(self, context):
 		layout = self.layout
-    
 		scene = context.scene
 		active = bpy.context.active_object
-		
 		if "openbf-data" in active:
 			layout.operator(UnOpenBFifyOperator.bl_idname)
 			if active.type == "LIGHT":
@@ -99,13 +112,19 @@ class CustomPropOpenBFPanel(bpy.types.Panel):
 				if active.rigid_body != None:
 					layout.operator("rigidbody.object_remove")
 					layout.prop(active.rigid_body, 'collision_shape')
-					layout.label(text=" Enabled = Dynamic, Disabled = Static")
-					layout.prop(active.rigid_body, "enabled")
-					layout.prop(active.rigid_body, 'mass')
-					layout.prop(active.rigid_body, 'friction')
-					layout.prop(active.rigid_body, 'restitution')
+					#layout.label(text=" Enabled = Dynamic, Disabled = Static")
+					layout.prop(active.rigid_body, "enabled", text="Dynamic")
+					if active.rigid_body.enabled:
+						layout.prop(active.rigid_body, 'mass')
+						layout.prop(active.rigid_body, 'friction')
+						layout.prop(active.rigid_body, 'restitution')
 				else:
 					layout.operator("rigidbody.object_add")
+					layout.prop(active.openbfextras, "isParentCollisionMesh")
+					if active.openbfextras.isParentCollisionMesh:
+						layout.prop(active.openbfextras, "parentCollisionShape")
+			layout.prop(active.openbfextras, "hideInGame")
+			layout.operator(OpenBFValidateUserDataOperator.bl_idname)
 		else:
 			layout.operator(OpenBFifyOperator.bl_idname)
 
@@ -122,7 +141,6 @@ if onSceneUpdateKey in driver_namespace:
 
 	#if listener is in update listeners
 	if driver_namespace[onSceneUpdateKey] in depsgraph_update_post:
-		print("Listener in listeners")
 		#Remove listener
 		depsgraph_update_post.remove(driver_namespace[onSceneUpdateKey])
 
@@ -135,15 +153,32 @@ depsgraph_update_post.append(onSceneUpdate)
 #Add a driver for the listener
 driver_namespace[onSceneUpdateKey] = onSceneUpdate
 
+class OpenBFExtras(bpy.types.PropertyGroup):
+	isParentCollisionMesh : BoolProperty(name="Is Collision Mesh", description="Use as a collision mesh", default=False)
+	hideInGame : BoolProperty(name="Hide In-Game", description="Should be hidden or not", default=False)
+	parentCollisionShape : EnumProperty(items=[
+	("MESH", "Tri-Mesh", "Not very optimal and only a few colliders supported"),
+	("CONVEX_HULL", "Convex-Mesh", "Optimal, doesn't support mesh caves"),
+	("BOX", "Box", "A box shape - very optimal"),
+	("PLANE", "Plane", "A plane shape - very optimal"),
+	("SPHERE", "Sphere", "A sphere shape - very optimal"),
+	], name="Shape Type", description="Shape type to use", default="MESH")
+
 def register():
 	bpy.utils.register_class(CustomPropOpenBFPanel)
 	bpy.utils.register_class(OpenBFifyOperator)
 	bpy.utils.register_class(UnOpenBFifyOperator)
+	bpy.utils.register_class(OpenBFValidateUserDataOperator)
+	bpy.utils.register_class(OpenBFExtras)
+	bpy.types.Object.openbfextras = bpy.props.PointerProperty(type=OpenBFExtras)
 
 def unregister():
 	bpy.utils.unregister_class(CustomPropOpenBFPanel)
 	bpy.utils.unregister_class(OpenBFifyOperator)
 	bpy.utils.unregister_class(UnOpenBFifyOperator)
+	bpy.utils.unregister_class(OpenBFValidateUserDataOperator)
+	bpy.utils.unregister_class(OpenBFExtras)
+	del bpy.types.Object.openbfextras
 
 if __name__ == "__main__":
 	register()
